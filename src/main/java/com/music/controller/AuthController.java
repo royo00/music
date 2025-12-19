b57f76ff462d3eafc6065c69969af65dd3b3499a
royo00 <royo00@qq.com>
2025-12-19 15:12:54 +0800

    feat(auth):add admin and actor login

--- src/main/java/com/music/controller/AuthController.java
+++ src/main/java/com/music/controller/AuthController.java
@@ -18,6 +18,8 @@ import java.time.LocalDateTime;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    private final String username = "admin";
+    private  final String password = "admin123";
 
     private final UserMapper userMapper;
     private final JwtUtil jwtUtil;
@@ -71,7 +73,7 @@ public class AuthController {
         user.setUsername(dto.getUsername());
         user.setPassword(passwordEncoder.encode(dto.getPassword()));
         user.setEmail(dto.getEmail());
-        user.setRole(Constants.UserRole.USER);
+        user.setRole(dto.getRole());
         user.setStatus(Constants.UserStatus.ENABLED);
 
         LocalDateTime now = LocalDateTime.now(); // 获取当前系统时间
@@ -80,5 +82,58 @@ public class AuthController {
 
         userMapper.insert(user);
         return Result.success();
+    }
+    //管理员登录
+    @PostMapping("/admin")
+    public Result<String> admin(@RequestBody LoginDTO loginDTO) {
+        // 验证是否是管理员账号
+        if (!username.equals(loginDTO.getUsername()) ||
+                !password.equals(loginDTO.getPassword())) {
+            return Result.error("管理员账号或密码错误!");
+        }
+
+        // 查询用户信息
+        User user = new User();
+        user.setId(0L);
+        user.setUsername("admin");
+        user.setRole("admin");
+
+        // 生成 token
+        String token = jwtUtil.generateToken(
+                user.getId(),
+                user.getUsername(),
+                user.getRole()
+        );
+
+        return Result.success(token);
+    }
+    /**
+     * 艺人登录
+     */
+    @PostMapping("/actor")
+    public Result<String> actor(@RequestBody LoginDTO dto) {
+        User user = userMapper.findByUsername(dto.getUsername());
+
+        // 用户不存在或被禁用
+        if (user == null || !Constants.UserStatus.ENABLED.equals(user.getStatus())) {
+            return Result.error(Constants.ResponseMessage.UNAUTHORIZED);
+        }
+
+        // 密码错误
+        if (!passwordEncoder.matches(dto.getPassword(), user.getPassword())) {
+            return Result.error("密码错误");
+        }
+
+        // 非艺人账号
+        if(!user.getRole().equals(Constants.UserRole.ACTOR)){
+            return Result.error("请使用艺人账号");
+        }
+        String token = jwtUtil.generateToken(
+                user.getId(),
+                user.getUsername(),
+                user.getRole()
+        );
+
+        return Result.success(token);
     }
 }
